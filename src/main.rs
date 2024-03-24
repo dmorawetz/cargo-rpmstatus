8e1513c81b73af00211a1cbddc12db852a423d5c
Daniel Morawetz <daniel@morawetz.dev>
2024-03-24 17:02:58 +0100

    Improve manpage generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write;
 use std::path::Path;
 
 use args::RpmArgs;
@@ -40,12 +43,37 @@ fn main() -> Result<(), Error> {
 fn generate_manpage() -> anyhow::Result<()> {
     let cmd = RpmArgs::command();
     let man = clap_mangen::Man::new(cmd);
-    man.generate_to(Path::new("."))?;
-
-    // let mut buffer: Vec<u8> = Default::default();
-    // man.render(&mut buffer)?;
-
-    // std::fs::write(out_dir.join("mybin.1"), buffer)?;
-
+    let file = File::create(Path::new("cargo-rpmstatus.1"))?;
+    let mut writer = BufWriter::new(file);
+    man.render_title(&mut writer)?;
+    man.render_name_section(&mut writer)?;
+    let synopsis = r"
+.SH SYNOPSYS
+.PP
+\f[B]cargo rpmstatus\f[R] \f[B]OPTIONS\f[R]
+.PP
+\f[B]cargo rpmstatus --all-features\f[R]
+.PP
+\f[B]cargo rpmstatus -h\f[R]
+";
+    write!(writer, "{}", synopsis)?;
+    man.render_description_section(&mut writer)?;
+    man.render_options_section(&mut writer)?;
+    let addendum = r"
+.SH AUTHORS
+.TP
+Daniel Morawetz <daniel@morawetz.dev>
+Adapted this manpage from the cargo-debstatus package.
+.TP
+Matthias Geiger <matthias.geiger1024@tutanota.de>
+Wrote the manpage for the Debian system.
+.SH COPYRIGHT
+.PP
+Copyright \[co] 2024 Daniel Morawetz
+.PP
+Permission is granted to copy, distribute and/or modify this document
+under the terms of the GNU General Public License, Version 3 or (at your
+option) any later version published by the Free Software Foundation.";
+    write!(writer, "{}", addendum)?;
     Ok(())
 }
